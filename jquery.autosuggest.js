4d680c6d5c590d7263f58c1d5c4b955d4e09f7e7
geoffreyoliver <geoffreyokongo2@gmail.com>
2016-07-01 17:13:44 +0300

    Updating plugin code version number to v1.3.0

--- jquery.autosuggest.js
+++ jquery.autosuggest.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery AutoSuggest Plugin v1.2.0
+ * jQuery AutoSuggest Plugin v1.3.0
  * https://github.com/GeoffreyOliver/jquery-autosuggest
  * 
  * This plugin provides autosuggestion while typing into a textbox input field
